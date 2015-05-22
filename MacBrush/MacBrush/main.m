45602257c7bfc17ecff19d86e25ba22f33493f30
Nils Tekampe <nils@tekampe.org>
2015-05-22 14:39:30 +0200

    inital project

---
+++ MacBrush/MacBrush/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MacBrush
+//
+//  Created by Nils Tekampe on 22.05.15.
+//  Copyright (c) 2015 Nils Tekampe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
