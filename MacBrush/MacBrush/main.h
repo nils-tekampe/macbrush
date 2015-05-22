49d94c73862de165dbea3bbbced5932662f84034
Nils Tekampe <nils@tekampe.org>
2015-05-22 14:44:30 +0200

    added heade file for main

---
+++ MacBrush/MacBrush/main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  MacBrush
+//
+//  Created by Nils Tekampe on 22.05.15.
+//  Copyright (c) 2015 Nils Tekampe. All rights reserved.
+//
+
+#ifndef MacBrush_main_h
+#define MacBrush_main_h
+
+
+#endif
