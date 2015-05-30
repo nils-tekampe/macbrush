772bc670114bf5a8beeb9b724b7c6991a9a041be
Nils Tekampe <nils@tekampe.org>
2015-05-30 17:50:51 +0200

    added new empty project

---
+++ macbrush/macbrush/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  macbrush
+//
+//  Created by Nils Tekampe on 30.05.15.
+//  Copyright (c) 2015 Nils Tekampe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
