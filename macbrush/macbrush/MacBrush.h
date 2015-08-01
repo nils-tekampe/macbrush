6e375876099cdd696e02069a64c6a17f89851367
Nils Tekampe <nils@tekampe.org>
2015-08-01 12:45:44 +0200

    Starting OO implementation

---
+++ macbrush/macbrush/MacBrush.h
@@ -0,0 +1,13 @@
+//
+//  MacBrush.h
+//  macbrush
+//
+//  Created by Nils Tekampe on 01.08.15.
+//  Copyright (c) 2015 Nils Tekampe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MacBrush : NSObject
+
+@end
---
+++ macbrush/macbrush/MacBrush.m
@@ -0,0 +1,13 @@
+//
+//  MacBrush.m
+//  macbrush
+//
+//  Created by Nils Tekampe on 01.08.15.
+//  Copyright (c) 2015 Nils Tekampe. All rights reserved.
+//
+
+#import "MacBrush.h"
+
+@implementation MacBrush
+
+@end
